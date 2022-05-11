20d3d7f72948a2b0a493fcbb4a8cfb3b1c95aeda
Ross Whitfield <ross.whitfield@gmail.com>
2022-05-11 14:58:18 -0400

    Add clock

--- time.js
+++ time.js
@@ -1,5 +1,20 @@
 (() => {
-    timer = setInterval(function() {
-        document.body.innerHTML = `Current time is ${new Date()}`;
-    }, 1000);
-})()
+  timer = setInterval(function () {
+    let now = new Date();
+    const seconds = now.getSeconds() + now.getMilliseconds() / 1000;
+    const minutes = now.getMinutes() + seconds / 60;
+    const hours = now.getHours() + minutes / 60;
+
+    document
+      .getElementById("hour")
+      .setAttribute("transform", `rotate(${(hours / 12) * 360})`);
+    document
+      .getElementById("minute")
+      .setAttribute("transform", `rotate(${(minutes / 60) * 360})`);
+    document
+      .getElementById("second")
+      .setAttribute("transform", `rotate(${(seconds / 60) * 360})`);
+
+    document.getElementById("time").innerHTML = `Current time is ${now}`;
+  }, 40);
+})();
